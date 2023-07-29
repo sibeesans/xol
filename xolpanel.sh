42b7e379c13fcccf2e8e74a23ef63e157ef6fd38
sibeesans <105978585+sibeesans@users.noreply.github.com>
2023-07-29 08:11:44 +0700

    Update xolpanel.sh

--- xolpanel.sh
+++ xolpanel.sh
@@ -4,7 +4,7 @@
 rm -rf xolpanel.sh
 apt update && apt upgrade
 apt install python3 python3-pip git
-git clone https://github.com/Jengkoltunnel/xolpanel.git
+git clone https://github.com/askykenza/xolpanel.git
 unzip xolpanel/xolpanel.zip
 pip3 install -r xolpanel/requirements.txt
 pip3 install pillow
